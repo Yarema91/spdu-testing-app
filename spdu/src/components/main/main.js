c1250a1f6a0c593ce70ffbc6d08f04a086ce97a2
Yarema <yaremaphoto@gmail.com>
2022-07-14 19:57:10 +0300

    create main component

---
+++ spdu/src/components/main/main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Message from './message/message'
+
+export const Main = () => {
+    return (
+        <div>main
+            <Message />
+        </div>
+    )
+}
+export default Main
