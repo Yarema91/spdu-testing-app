b643fdad78bbf8c0a2949a2cc366dc191bbe9ee6
Yarema <yaremaphoto@gmail.com>
2022-07-14 20:05:39 +0300

    add style + function main

--- spdu/src/components/main/main.js
+++ spdu/src/components/main/main.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import Message from './message/message'
+import React, { useState } from 'react';
+
+import './main.css';
+import { Message } from './message/message';
+
+export const INITIAL_MESSAGES = [
+    { text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', isCurrentUser: true },
+    { text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', isCurrentUser: false },
+    { text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', isCurrentUser: true }
+];
 
 export const Main = () => {
+    const [messages, setMessages] = useState(INITIAL_MESSAGES);
+    const [currentMessage, setCurrentMessage] = useState('');
+
+    const onTextAreaChange = ({ target: { value } }) => {
+        setCurrentMessage(value);
+    };
+
     return (
-        <div>main
-            <Message />
+        <div className="main">
+            <div className="main__messages">
+                {messages.map(({ text, isCurrentUser }, i) => (
+                    <Message key={i} text={text} isCurrentUser={isCurrentUser} />
+                ))}
+            </div>
+            <div className="main__plate">
+                <textarea className="main__textarea" onChange={onTextAreaChange} value={currentMessage} />
+            </div>
+            <div className="main__plate">
+                <button className="main__button">Send message</button>
+            </div>
         </div>
     )
-}
-export default Main
+};
+
+export default Main;
